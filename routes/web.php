e45f9104f7d35c95bafb9ea7b29c1f6564207921
IrsyadArsyaduta <ysusetyarini@gmail.com>
2024-07-01 16:59:34 +0700

    isi project

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DataSiswaController;
+use App\Http\Controllers\DatasiswaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route resource for DataSiswa
-Route::resource('datasiswa', DataSiswaController::class);
+// Define resource routes for DatasiswaController
+Route::get('/datasiswa', [DatasiswaController::class, 'index'])->name('datasiswa.index');
+Route::post('/datasiswa/store', [DatasiswaController::class, 'store'])->name('datasiswa.store');
+Route::get('/datasiswa/{id}', [DatasiswaController::class, 'destroy'])->name('datasiswa.destroy');
+Route::put('/datasiswa/{id}', [DatasiswaController::class, 'update'])->name('datasiswa.update');
+Route::get('/datasiswa/search', [DatasiswaController::class, 'search'])->name('datasiswa.search');
+
